ee3d0960655606466a1b113b765780176821c4ba
JesusG2000 <fpfpfppfp@mail.ru>
2021-05-31 15:36:21 +0300

    add service models

---
+++ internal/service/file.go
@@ -0,0 +1,149 @@
+package service
+
+import (
+	"context"
+
+	"github.com/JesusG2000/hexsatisfaction_purchase/internal/model"
+	"github.com/JesusG2000/hexsatisfaction_purchase/internal/repository"
+	"github.com/pkg/errors"
+)
+
+// FileService is a file service.
+type FileService struct {
+	repository.File
+}
+
+// NewFileService is a FileService service constructor.
+func NewFileService(file repository.File) *FileService {
+	return &FileService{file}
+}
+
+// Create creates new file and returns id.
+func (f FileService) Create(ctx context.Context, request model.CreateFileRequest) (string, error) {
+	file := model.FileDTO{
+		Name:        request.Name,
+		Description: request.Description,
+		Size:        request.Size,
+		Path:        request.Path,
+		AddDate:     request.AddDate,
+		UpdateDate:  request.UpdateDate,
+		Actual:      request.Actual,
+		AuthorID:    request.AuthorID,
+	}
+	id, err := f.File.Create(ctx, file)
+	if err != nil {
+		return "", errors.Wrap(err, "couldn't create file")
+	}
+
+	return id, nil
+}
+
+// Update updates file and returns id.
+func (f FileService) Update(ctx context.Context, request model.UpdateFileRequest) (string, error) {
+	file := model.FileDTO{
+		Name:        request.Name,
+		Description: request.Description,
+		Size:        request.Size,
+		Path:        request.Path,
+		AddDate:     request.AddDate,
+		UpdateDate:  request.UpdateDate,
+		Actual:      request.Actual,
+		AuthorID:    request.AuthorID,
+	}
+	id, err := f.File.Update(ctx, request.ID, file)
+	if err != nil {
+		return "", errors.Wrap(err, "couldn't update file")
+	}
+
+	return id, nil
+}
+
+// Delete deletes file and returns deleted id.
+func (f FileService) Delete(ctx context.Context, request model.DeleteFileRequest) (string, error) {
+	id, err := f.File.Delete(ctx, request.ID)
+	if err != nil {
+		return "", errors.Wrap(err, "couldn't delete file")
+	}
+
+	return id, nil
+}
+
+// FindByID finds file by id.
+func (f FileService) FindByID(ctx context.Context, request model.IDFileRequest) (*model.FileDTO, error) {
+	file, err := f.File.FindByID(ctx, request.ID)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find file")
+	}
+
+	return file, nil
+}
+
+// FindByName finds files by name.
+func (f FileService) FindByName(ctx context.Context, request model.NameFileRequest) ([]model.FileDTO, error) {
+	files, err := f.File.FindByName(ctx, request.Name)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find files")
+	}
+
+	return files, nil
+}
+
+// FindAll finds files.
+func (f FileService) FindAll(ctx context.Context) ([]model.FileDTO, error) {
+	files, err := f.File.FindAll(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find files")
+	}
+
+	return files, nil
+}
+
+// FindByAuthorID finds files by author id.
+func (f FileService) FindByAuthorID(ctx context.Context, request model.AuthorIDFileRequest) ([]model.FileDTO, error) {
+	files, err := f.File.FindByAuthorID(ctx, request.ID)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find files")
+	}
+
+	return files, nil
+}
+
+// FindNotActual finds not actual files.
+func (f FileService) FindNotActual(ctx context.Context) ([]model.FileDTO, error) {
+	files, err := f.File.FindNotActual(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find files")
+	}
+
+	return files, nil
+}
+
+// FindActual finds actual files.
+func (f FileService) FindActual(ctx context.Context) ([]model.FileDTO, error) {
+	files, err := f.File.FindActual(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find files")
+	}
+
+	return files, nil
+}
+
+// FindAddedByPeriod finds added files by date period.
+func (f FileService) FindAddedByPeriod(ctx context.Context, request model.AddedPeriodFileRequest) ([]model.FileDTO, error) {
+	files, err := f.File.FindAddedByPeriod(ctx, request.Start, request.End)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find files")
+	}
+
+	return files, nil
+}
+
+// FindUpdatedByPeriod finds updated files by date period.
+func (f FileService) FindUpdatedByPeriod(ctx context.Context, request model.UpdatedPeriodFileRequest) ([]model.FileDTO, error) {
+	files, err := f.File.FindUpdatedByPeriod(ctx, request.Start, request.End)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find files")
+	}
+
+	return files, nil
+}
---
+++ internal/service/purchase.go
@@ -0,0 +1,174 @@
+package service
+
+import (
+	"context"
+
+	"github.com/JesusG2000/hexsatisfaction_purchase/internal/model"
+	"github.com/JesusG2000/hexsatisfaction_purchase/internal/repository"
+	"github.com/pkg/errors"
+)
+
+// PurchaseService is a purchase service.
+type PurchaseService struct {
+	repository.Purchase
+}
+
+// NewPurchaseService is a PurchaseService service constructor.
+func NewPurchaseService(purchase repository.Purchase) *PurchaseService {
+	return &PurchaseService{purchase}
+}
+
+// Create creates new purchase and returns id.
+func (p PurchaseService) Create(ctx context.Context, request model.CreatePurchaseRequest) (string, error) {
+	purchase := model.PurchaseDTO{
+		UserID: request.UserID,
+		Date:   request.Date,
+		FileID: request.FileID,
+	}
+	id, err := p.Purchase.Create(ctx, purchase)
+	if err != nil {
+		return "", errors.Wrap(err, "couldn't create purchase")
+	}
+
+	return id, nil
+}
+
+// Delete deletes purchase and returns deleted id.
+func (p PurchaseService) Delete(ctx context.Context, request model.DeletePurchaseRequest) (string, error) {
+	id, err := p.Purchase.Delete(ctx, request.ID)
+	if err != nil {
+		return "", errors.Wrap(err, "couldn't delete purchase")
+	}
+
+	return id, nil
+}
+
+// FindByID finds purchase by id.
+func (p PurchaseService) FindByID(ctx context.Context, request model.IDPurchaseRequest) (*model.PurchaseDTO, error) {
+	purchase, err := p.Purchase.FindByID(ctx, request.ID)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find purchase")
+	}
+
+	return purchase, nil
+}
+
+// FindLastByUserID finds last purchase by user id.
+func (p PurchaseService) FindLastByUserID(ctx context.Context, request model.UserIDPurchaseRequest) (*model.PurchaseDTO, error) {
+	purchase, err := p.Purchase.FindLastByUserID(ctx, request.ID)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find purchase")
+	}
+
+	return purchase, nil
+}
+
+// FindAllByUserID finds purchases by user id.
+func (p PurchaseService) FindAllByUserID(ctx context.Context, request model.UserIDPurchaseRequest) ([]model.PurchaseDTO, error) {
+	purchases, err := p.Purchase.FindAllByUserID(ctx, request.ID)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find purchases")
+	}
+
+	return purchases, nil
+}
+
+// FindByUserIDAndPeriod finds purchases by user id and date period.
+func (p PurchaseService) FindByUserIDAndPeriod(ctx context.Context, request model.UserIDPeriodPurchaseRequest) ([]model.PurchaseDTO, error) {
+	purchases, err := p.Purchase.FindByUserIDAndPeriod(ctx, request.ID, request.Start, request.End)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find purchases")
+	}
+
+	return purchases, nil
+}
+
+// FindByUserIDAfterDate finds purchases by user id and after date.
+func (p PurchaseService) FindByUserIDAfterDate(ctx context.Context, request model.UserIDAfterDatePurchaseRequest) ([]model.PurchaseDTO, error) {
+	purchases, err := p.Purchase.FindByUserIDAfterDate(ctx, request.ID, request.Start)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find purchases")
+	}
+
+	return purchases, nil
+}
+
+// FindByUserIDBeforeDate finds purchases by user id and before date.
+func (p PurchaseService) FindByUserIDBeforeDate(ctx context.Context, request model.UserIDBeforeDatePurchaseRequest) ([]model.PurchaseDTO, error) {
+	purchases, err := p.Purchase.FindByUserIDBeforeDate(ctx, request.ID, request.End)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find purchases")
+	}
+
+	return purchases, nil
+}
+
+// FindByUserIDAndFileID finds purchases by user id and file id.
+func (p PurchaseService) FindByUserIDAndFileID(ctx context.Context, request model.UserIDFileIDPurchaseRequest) ([]model.PurchaseDTO, error) {
+	purchases, err := p.Purchase.FindByUserIDAndFileID(ctx, request.UserID, request.FileID)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find purchases")
+	}
+
+	return purchases, nil
+}
+
+// FindLast finds last purchase.
+func (p PurchaseService) FindLast(ctx context.Context) (*model.PurchaseDTO, error) {
+	purchase, err := p.Purchase.FindLast(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find purchase")
+	}
+
+	return purchase, nil
+}
+
+// FindAll finds purchases.
+func (p PurchaseService) FindAll(ctx context.Context) ([]model.PurchaseDTO, error) {
+	purchases, err := p.Purchase.FindAll(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find purchases")
+	}
+
+	return purchases, nil
+}
+
+// FindByPeriod finds purchases by date period.
+func (p PurchaseService) FindByPeriod(ctx context.Context, request model.PeriodPurchaseRequest) ([]model.PurchaseDTO, error) {
+	purchases, err := p.Purchase.FindByPeriod(ctx, request.Start, request.End)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find purchases")
+	}
+
+	return purchases, nil
+}
+
+// FindAfterDate finds purchases after date.
+func (p PurchaseService) FindAfterDate(ctx context.Context, request model.AfterDatePurchaseRequest) ([]model.PurchaseDTO, error) {
+	purchases, err := p.Purchase.FindAfterDate(ctx, request.Start)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find purchases")
+	}
+
+	return purchases, nil
+}
+
+// FindBeforeDate finds purchases before date.
+func (p PurchaseService) FindBeforeDate(ctx context.Context, request model.BeforeDatePurchaseRequest) ([]model.PurchaseDTO, error) {
+	purchases, err := p.Purchase.FindBeforeDate(ctx, request.End)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find purchases")
+	}
+
+	return purchases, nil
+}
+
+// FindByFileID finds purchases by file id.
+func (p PurchaseService) FindByFileID(ctx context.Context, request model.FileIDPurchaseRequest) ([]model.PurchaseDTO, error) {
+	purchases, err := p.Purchase.FindByFileID(ctx, request.FileID)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find purchases")
+	}
+
+	return purchases, nil
+}
